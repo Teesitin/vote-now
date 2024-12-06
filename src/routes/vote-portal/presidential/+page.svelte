f848cff077fff988c10391446640bed8465fb80f
DeLayne Russell <teesitin.russell@gmail.com>
2024-12-05 20:37:17 -0500

    Update +page.svelte

--- src/routes/vote-portal/presidential/+page.svelte
+++ src/routes/vote-portal/presidential/+page.svelte
@@ -105,7 +105,7 @@
         </label>
 
 
-        <button class="flex justify-center items-center w-fit bg-old-glory-blue p-3 px-7 rounded-xl text-white font-bold hover:bg-old-glory-blue/50" on:click={startFacialScan}>
+        <button class="flex justify-center items-center w-fit bg-old-glory-blue p-3 px-7 rounded-xl text-white font-bold hover:bg-old-glory-blue/80" on:click={startFacialScan}>
             {#if facialSuccess}
                 Facial Recognition <svg class="ml-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
             {:else}
@@ -114,18 +114,29 @@
         </button>
     </div>
 
-    <button 
-        class="mt-20 flex justify-center items-center w-full bg-old-glory-red p-3 rounded-xl text-white font-bold hover:bg-old-glory-red/50 {allFieldsFilled ? '' : 'bg-gray-300'}"
-        disabled={allFieldsFilled}
-    >
-        Complete Voter Sign In
-    </button>
+    {#if allFieldsFilled}
+        <button 
+            class="mt-20 flex justify-center items-center w-full bg-old-glory-red p-3 rounded-xl text-white font-bold hover:bg-old-glory-red/80 cursor-pointer"
+            disabled={allFieldsFilled}
+        >
+            Complete Voter Sign In
+        </button>
+    {:else}
+        <div class="text-lg text-center mt-10 text-gray-400 font-bold">Please fill every field and complete facial recognition</div>
+    {/if}
 </div>
 
 {#if facialModal}
     <div class="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur">
-        <div class="m-auto w-96 h-96 bg-white border-2 rounded-xl flex flex-col items-center justify-center">
-            {#if loading}
+        <div class="relative m-auto w-96 h-96 bg-white border-2 rounded-xl flex flex-col items-center justify-center">
+    
+            {#if !loading && successMessage}
+                <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#16a34a"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                <p class="text-green-600 font-bold mt-5">{successMessage}</p>
+                <button class="flex justify-center mt-10 items-center w-fit bg-green-600 p-3 px-7 rounded-xl text-white font-bold hover:bg-green-600/80" on:click={() => facialModal = false}>
+                    Close
+                </button>
+            {:else}
                 <div class="flex items-center gap-2">
                     <div class="w-5 h-5 bg-blue-500 rounded-full animate-bounce"></div>
                     <div class="w-5 h-5 bg-blue-500 rounded-full animate-bounce delay-150"></div>
@@ -134,15 +145,9 @@
                 <p class="mt-5 text-gray-700 font-bold">Scanning...</p>
             {/if}
 
-            {#if !loading && successMessage}
-                <p class="text-green-600 font-bold mb-5">{successMessage}</p>
-                <button 
-                    class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600" 
-                    on:click={closeModal}
-                >
-                    Close
-                </button>
-            {/if}
+            <button class="absolute top-2 right-2 rounded-full bg-gray-300 hover:bg-gray-200 p-1 w-6 h-6 flex justify-center items-center" on:click={() => facialModal = false}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+            </button>
         </div>
     </div>
 {/if}
